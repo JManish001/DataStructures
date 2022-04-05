4cb337b2385a589d5bdbb0625089400b4a2afb6b
Manish Jindal <99240049+JManish001@users.noreply.github.com>
2022-04-05 10:19:23 +0530

    Add files via upload

---
+++ array/01_Arrays_Insertion.c
@@ -0,0 +1,44 @@
+#include<stdio.h>
+
+ void display(int arr[],int n)
+{
+    for(int i=0;i<n;i++)
+    {
+        printf("%d\n",arr[i]);
+    }
+}
+int Insertion(int arr[],int size,int element,int capacity,int index)
+{
+
+    if(size>= capacity)
+    {
+        return -1;
+    }
+    for(int i=size-1;i>=index;i--)
+    {
+
+        arr[i+1]=arr[i];
+
+    }
+    arr[index] =element;
+    return 1;
+
+}
+
+int main()
+{
+    int arr[100]={1,2,3,4,5};
+    int size=5 ,element =6,index=2;
+    printf("Before Insertion :\n");
+    display(arr,size);
+
+    Insertion(arr,size,element,100,index);
+    size =size+1;
+    printf("After Insertion :\n");
+    display(arr,size);
+
+
+
+
+}
+
